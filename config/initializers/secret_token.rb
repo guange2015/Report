81485c69a75a7cde447bb805b341c8be071b07a5
hhuai <8863824@gmail.com>
2011-01-12 10:41:33 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Report::Application.config.secret_token = '2c142401a4b95f71048a27470d3c4fa181cc6efdbbd67da656c77243644b22b0606babbec1fdb051671dad6b17868e8d9dd3dfa27b7ad9992c3f6d047f333bcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Report::Application.config.session_store :cookie_store, :key => '_Report_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Report::Application.config.session_store :active_record_store
