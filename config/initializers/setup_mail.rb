7745a5f10718667e863dc3fc001b7c1fdd0dc766
hhuai <8863824@gmail.com>
2011-01-22 15:52:47 +0800

    ..

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.smtp_settings ={
 	:domain => 'talkweb.com.cn',
 	:port => '25',
 	:user_name => 'huangxiaoguang',
-	:password => 'china20081',
+	:password => 'china2008',
 	:authentication => :login
 }
 ADMINEMAIL = 'huangxiaoguang@talkweb.com.cn'
